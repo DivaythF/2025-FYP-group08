8fbcb4e22e3bad8ddb5d43ee487dadd506da9499
O´NEAL <onok@itu.dk>
2025-02-26 10:25:04 +0100

    Update code.py
    all the codes

--- code.py
+++ code.py
@@ -2,7 +2,29 @@ import random
 
 import cv2
 
+class ImageDataLoader:
+    def __init__(self, directory, image_size=(224, 224)):
+        self.directory = directory
+        self.image_size = image_size
+        self.image_paths = self._get_image_paths()
+        
+    def _get_image_paths(self):
+        
+        valid_extensions = ['.jpg', '.jpeg', '.png', '.bmp']
+        image_paths = []
 
+        for filename in os.listdir(self.directory):
+            full_path = os.path.join(self.directory, filename)
+        
+           
+
+        # Check if the file is an image
+            if any(filename.lower().endswith(ext) for ext in valid_extensions):
+                image_paths.append(full_path)
+                
+
+        return image_paths
+        
 def readImageFile(file_path):
     # read image as an 8-bit array
     img_bgr = cv2.imread(file_path)
@@ -31,26 +53,17 @@ def saveImageFile(img_rgb, file_path):
         print(f"Error saving the image: {e}")
         return False
 
+def removeHair(img_org, img_gray, kernel_size=25, threshold=10, radius=3):
+    # kernel for the morphological filtering
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (kernel_size, kernel_size))
 
-class ImageDataLoader:
-    def __init__(self, directory, image_size=(224, 224)):
-        self.directory = directory
-        self.image_size = image_size
-        self.image_paths = self._get_image_paths()
-        
-    def _get_image_paths(self):
-        
-        valid_extensions = ['.jpg', '.jpeg', '.png', '.bmp']
-        image_paths = []
+    # perform the blackHat filtering on the grayscale image to find the hair countours
+    blackhat = cv2.morphologyEx(img_gray, cv2.MORPH_BLACKHAT, kernel)
 
-        for filename in os.listdir(self.directory):
-            full_path = os.path.join(self.directory, filename)
-        
-           
+    # intensify the hair countours in preparation for the inpainting algorithm
+    _, thresh = cv2.threshold(blackhat, threshold, 255, cv2.THRESH_BINARY)
 
-        # Check if the file is an image
-            if any(filename.lower().endswith(ext) for ext in valid_extensions):
-                image_paths.append(full_path)
-                
+    # inpaint the original image depending on the mask
+    img_out = cv2.inpaint(img_org, thresh, radius, cv2.INPAINT_TELEA)
 
-        return image_paths
+    return blackhat, thresh, img_out
