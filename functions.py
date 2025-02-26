7cd05fbae7ab951467204ce1a35c309f4194410d
Kagemand32 <hjalte.e.t@gmail.com>
2025-02-26 11:42:20 +0100

    added iterative hair removal

--- functions.py
+++ functions.py
@@ -1,5 +1,5 @@
 import random
-
+import os
 import cv2
 
 class ImageDataLoader:
--- hairremovaltest.py
+++ hairremovaltest.py
@@ -1,9 +1,15 @@
 import functions as fun 
-import cv2 
+import cv2
+
+#get paths and save in some list
+imagePaths = fun.ImageDataLoader("data")._get_image_paths()
+#print(imagePaths)
+
+#loop through original images, remove hair and save in different directory
+for i in range(len(imagePaths)):
+    rgb, grey = fun.readImageFile(imagePaths[i])
+    _, _, img = fun.removeHair(rgb, grey)
+    fun.saveImageFile(img, f"hairRemoval/removed_{917+i}.png")
 
 
-rgb, grey = fun.readImageFile("data/img_0918.png")
-
-blackhat, thresh, img_out = fun.removeHair( rgb, grey, kernel_size=25, threshold=10, radius=3)
-fun.saveImageFile(img_out, "test_removal/someImage.png")
 
